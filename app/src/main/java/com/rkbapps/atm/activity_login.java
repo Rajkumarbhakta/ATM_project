13129a4d9ad32c09c6fb06dcce4d5e196bffd68b
Rajkumar Bhakta <email.rajkumarbhakta@gmail.com>
2022-08-26 16:19:16 +0530

    add text do you have account

--- app/src/main/java/com/rkbapps/atm/activity_login.java
+++ app/src/main/java/com/rkbapps/atm/activity_login.java
@@ -1,15 +1,30 @@
 package com.rkbapps.atm;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class activity_login extends AppCompatActivity {
+    EditText accountNumber,pin;
+    TextView openAccount;
+    Button submit;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        getSupportActionBar().hide();
+        getSupportActionBar().setTitle("Sign in");
+        accountNumber = findViewById(R.id.account_number);
+        pin=findViewById(R.id.pin);
+        openAccount=findViewById(R.id.newaccount);
+        submit = findViewById(R.id.login);
+
+
     }
 }
