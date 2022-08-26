2d6b30677b2b32068369fe1a9898bdc7439af89d
Atmakendric <bapanmandal86958@gmail.com>
2022-08-26 16:20:38 +0530

    change bg

--- app/src/main/java/com/rkbapps/atm/activity_login.java
+++ app/src/main/java/com/rkbapps/atm/activity_login.java
@@ -3,13 +3,24 @@ package com.rkbapps.atm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class activity_login extends AppCompatActivity {
-
+Button submit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         getSupportActionBar().hide();
+        submit=findViewById(R.id.login);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(activity_login.this, "Enter account number and PIN", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
